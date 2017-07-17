829f30922184cba15fd654b6e841cef9ee380cdc
RubenBranco <ruben.branco@outlook.pt>
2017-07-17 23:30:18 +0100

    Final parser

--- Handwriting Recognition/TensorFlow/README.md
+++
@@ -1,7 +0,0 @@
-# nn-project
-
-A repository that contains my efforts in learning how Neural Networks work. Their different architectures, activation functions,
-learning methods and the different tasks that can be achieved with one. The first ones will be done from scratch(other than the use
-of numpy of course), further ones will be used using a framework.
-
-All of this is done in Python 3.6.
--- Language Modelling/parser.py
+++ Language Modelling/parser.py
@@ -1,5 +1,6 @@
 import json
 import re
+import os
 
 
 class ChatParser:
@@ -11,18 +12,25 @@ class ChatParser:
         banned_accounts = ['wowsobot', 'twitchnotify', 'moobot', 'hnlbot', 'nightbot', 'alinity_bot', 'xanbot', 'amazquotebot', 'scamazbot', 'zebooom', 'aoaagoldbot', 'baconrobot', 'rept0bot', 'blackbadge', 'bobross', 'boxboxbot', 'carcibot', 'revlobot', 'chinglishtvbot', 'jayridebot', 'cluntbotstovens', 'cloaktato', 'cohhilitionbot', 'waffless_', 'laurieforman', 'jackieburkhart', 'desbot', 'destiny_bot', 'Bot_v2_Beta', 'Logs', 'purrbot', 'docb0t', 'niconicobot', 'supascootpm', 'exbcbot', 'berzekerbot', 'snusbot', 'fourtfbot', 'adeladeexe', 'ambot', 'stahpbot', 'itmebot', 'boterie', 'streamelements', 'kaybotify', 'thebanebot', 'lasskeepobot', 'lasskeepobot', 'loopy_the_merciful', 'lulabot', 'mtgbot', 'mandroid', 'gather_bot', 'onscreenbot', 'spitter_bot', 'proxymybot', 'analyticsbot', 'sheepfarmer', 'rrbot', 'wizebot', 'botmotion', 'startupbot', 'chinnbot', 'mithbot', 'swiftsbot', 'swiftpoints', 'pantsu__bot', 'priestbot', 'x9kbot', 'botseventeen', 'tpp', 'litzbot', 'ohbot', 'hoffmannbot', 'voyscout', 'sc2replaystatsbot', 'wobblerbot', 'jaffamod']
         with open(self.file) as file_reader:
             with open(write_file_name, 'w') as file_writer:
-                lines = json.loads(file_reader.readline())['text']
-                for line in lines.split('\n'):
-                    if len(line) > 0:
-                        match = re.findall('([\[\d]*-[\d]*-[\d]* [\d]*:[\d]*:[\d]* UTC\]) ([A-Za-z\d_]*)', line)
-                        slicer = len(match[0][0])+len(match[0][1])+3
-                        if match[0][1] not in banned_accounts and line[slicer:][0] != '!':
-                            if line[slicer:] != '<Message Deleted>':
-                                line = re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', '', line[slicer:])
-                                if line != '':
-                                    file_writer.write(line + '\n')
+                for line in file_reader:
+                    lines = json.loads(line)['text']
+                    for chat_line in lines.split('\n'):
+                        if len(chat_line) > 0:
+                            match = re.findall('([\[\d]*-[\d]*-[\d]* [\d]*:[\d]*:[\d]* UTC\]) ([A-Za-z\d_]*)', chat_line)
+                            slicer = len(match[0][0])+len(match[0][1])+3
+                            if len(chat_line[slicer:]) > 0:
+                                if match[0][1] not in banned_accounts and chat_line[slicer:][0] != '!':
+                                    if chat_line[slicer:] != '<Message Deleted>':
+                                        chat_line = re.sub(r'\w+:\/{2}[\d\w-]+(\.[\d\w-]+)*(?:(?:\/[^\s/]*))*', '', chat_line[slicer:])
+                                        chat_line = re.sub('@[A-Za-z\d_]*', '', chat_line)
+                                        if chat_line != '':
+                                            try:
+                                                file_writer.write(chat_line + '\n')
+                                            except:
+                                                new_line = chat_line.encode('ascii', 'ignore') + b'\n'
+                                                file_writer.write(new_line.decode('utf-8'))
 
 
 if __name__ == '__main__':
-    parser = ChatParser('/Volumes/LINUX MINT/twitchchat.jl')
+    parser = ChatParser(os.path.normpath('C:/Users/Ruben/Desktop/neuralnetwork/Neural-Network-Project/Language Modelling/Scraper/Scraper/twichlogs.jl'))
     parser.parse('twitch.log')
